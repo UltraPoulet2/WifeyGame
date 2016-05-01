0ef97f47d545bb576d4d77b67cd0b6b254fd55f5
UltraPoulet2 <ultrapoulet@gmail.com>
2016-05-01 15:13:59 -0500

    Remove Println from BattleScreen
    Also seeing what other things get committed

--- app/src/main/java/ultrapoulet/wifeygame/battle/BattleScreen.java
+++ app/src/main/java/ultrapoulet/wifeygame/battle/BattleScreen.java
@@ -798,7 +798,6 @@ public class BattleScreen extends Screen {
         if(currentPhase == BattlePhase.ANIMATE_ENEMY_ACTION){
             drawEnemyDamage();
         }
-        System.out.println("well then");
     }
 
     private void drawBackground(){
