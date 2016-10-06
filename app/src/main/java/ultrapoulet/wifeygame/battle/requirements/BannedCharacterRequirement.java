356b4ddacf0c442a1aae59744205c0562b9edb90
UltraPoulet2 <ultrapoulet@gmail.com>
2016-10-06 18:47:59 -0500

    Add Banned Character Requirement

---
+++ app/src/main/java/ultrapoulet/wifeygame/battle/requirements/BannedCharacterRequirement.java
@@ -0,0 +1,43 @@
+package ultrapoulet.wifeygame.battle.requirements;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import ultrapoulet.wifeygame.character.WifeyCharacter;
+import ultrapoulet.wifeygame.gamestate.RecruitedCharacters;
+
+/**
+ * Created by John on 10/6/2016.
+ */
+
+public class BannedCharacterRequirement extends AbsRequirement {
+
+    private ArrayList<WifeyCharacter> bannedWifeys;
+
+    public BannedCharacterRequirement(){
+        bannedWifeys = new ArrayList<>();
+    }
+
+    //Returns true if the value was successfully added, false otherwise
+    public boolean addValue(String input){
+        //Change this to all instead of recruited
+        WifeyCharacter wifey = RecruitedCharacters.get(input);
+        if(wifey == null){
+            return false;
+        }
+        if(bannedWifeys.contains(wifey)){
+            return false;
+        }
+        bannedWifeys.add(wifey);
+        return true;
+    }
+
+    //Returns true if the character is valid for this requirement
+    public boolean validateCharacter(WifeyCharacter character){
+        return !bannedWifeys.contains(character);
+    }
+
+    public String getDescription(){
+        return "To do";
+    }
+}
